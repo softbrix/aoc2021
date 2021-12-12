b77451a707d6e5d13c7a79059a9ceb1daa604040
Andreas Sehr <andreas@softbrix.se>
2021-12-12 17:39:31 +0100

    Day 12

--- 12/main.js
+++ 12/main.js
@@ -21,22 +21,23 @@ rl.on('line', (line) => {
 });
 
 rl.on('close', (input) => {
-  console.log(edges)
-  const sum1 = walkTheWalk('start', [], 0);
-  //let res = mt.map((row, y) => analyse(row, y));
-  //let sum1 = res.flat().reduce((t, i) => t + i, 0);
+  const sum1 = walkTheWalk('start', [], 0, -1);
   console.log(`Sum1: ${sum1}`);
-
-  //console.log(`Sum2: ${sum2}`);
+  const sum2 = walkTheWalk('start', [], 0, undefined);
+  console.log(`Sum2: ${sum2}`);
 });
 
 function isLower(str) {
   return str.toLowerCase() == str;
 }
 
-function walkTheWalk(node, visited, steps) {
+function walkTheWalk(node, visited, steps, memory) {
   if (visited.indexOf(node) >= 0) {
-    return 0;
+    if (node != "start" && memory === undefined) {
+      memory = node;
+    } else {
+      return 0;
+    }
   }
   if (node == "end") {
     return 1;
@@ -45,40 +46,6 @@ function walkTheWalk(node, visited, steps) {
     visited = visited.concat([node])
   }
   return edges[node]
-    .map(t => walkTheWalk(t, visited,++steps))
+    .map(t => walkTheWalk(t, visited,++steps, memory))
     .reduce((a, i) => a+i, 0)
-}
-
-function isLow(v, x, y) {
-  if (v < g(x-1, y) &&
-      v < g(x+1, y) &&
-      v < g(x, y-1) &&
-      v < g(x, y+1)) {
-        lows.push({x, y});
-        return v + 1;
-      }
-  return -1;
-}
-
-let visited = new Set();
-function calcBasins(p) {
-  let k = ''+p.x+','+p.y;
-  if (visited.has(k) || g(p.x,p.y) == 9) {
-    return 0;
-  }
-  visited.add(k);
-  let b = 1 +
-  calcBasins({x:p.x - 1, y:p.y}) +
-  calcBasins({x:p.x + 1, y:p.y}) + 
-  calcBasins({x:p.x,     y:p.y - 1}) +
-  calcBasins({x:p.x,     y:p.y + 1});
-  return  b;
-}
-
-function g(x, y) {
-  if (x < 0 || x >= mt[0].length ||
-    y < 0 || y >= mt.length) {
-    return 9;
-  }
-  return mt[y][x];
 }
